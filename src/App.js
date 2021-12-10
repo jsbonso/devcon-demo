a8c97ca7168ff43e0fe57b5304d863395a588912
Tutorials Dojo <jsbonso@gmail.com>
2021-12-10 17:12:29 +0800

    added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { createTodo, updateTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
-  );
+ const [allTodos, setAlltodos] = useState(null);
+
+ useEffect(() => {
+   (async () => {
+     const todos = await API.graphql(graphqlOperation(listTodos));
+     setAlltodos(todos.data.listTodos.items);
+   })();
+ }, []);
+
+ const [name, setTodoName] = useState("");
+
+ const changeTodoName = (e) => {
+   setTodoName(e.target.value);
+ };
+
+ const submitAddTodo = async (e) => {
+   e.preventDefault();
+   if (name === "") return alert("Input field cannot be empty");
+   const todo = { name, done: false };
+   await API.graphql(graphqlOperation(createTodo, { input: todo }));
+   allTodos === null ? setAlltodos([todo]) : setAlltodos([todo, ...allTodos]);
+ };
+
+ const toggleTodo = async (id) => {
+   const todo = allTodos.find(({ id: _id }) => _id === id);
+   let newTodo = { id, name: todo.name };
+   newTodo.done = todo.done ? false : true;
+   await API.graphql(graphqlOperation(updateTodo, { input: newTodo }));
+ };
+
+ return (
+
+   <div className="App">
+     <div className="heading">
+       <h1>Amplify Todo</h1>
+       
+     </div>
+     <form className="add-todo-form" onSubmit={submitAddTodo}>
+       <input
+         placeholder="Add Todo"
+         onChange={changeTodoName}
+       />
+       <button type="submit">+</button>
+     </form>
+     {allTodos === null ? (
+       <p>Loading Todos...</p>
+     ) : allTodos.length === 0 ? (
+       <p>No Todo available</p>
+     ) : (
+       <div className="todos">
+         {allTodos.reverse().map(({ id, name, done },i) => (
+           <div className="todo-block" key={i}>
+             <input
+               onClick={() => toggleTodo(id)}
+               type="checkbox"
+               id={id}
+               value={id}
+               key={i}
+               defaultChecked={done}
+             />
+             <label htmlFor={id}>{name}</label>
+           </div>
+         ))}
+       </div>
+     )}
+   </div>
+
+ );
 }
 
-export default withAuthenticator(App);
+export default App;
